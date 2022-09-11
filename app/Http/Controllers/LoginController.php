5576efaaec04f1c5af9a7e5abe4f07e83d3e892e
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2022-09-12 04:42:39 +0700

    rewrite code to get data for chart

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Car;
 use App\Customer;
+use App\Http\Controllers\Controller;
 use App\Transaction;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -34,44 +32,79 @@ class LoginController extends Controller
         return view('backend.component.login');
     }
 
-    public function dashboard(){
+    public function dashboard()
+    {
         $car = $this->car;
         $customer = $this->customer;
-        $transaction  = $this->transaction;
-        $transaction_data = [];
-        for($i=1;$i<=12;$i++){
-            $lul = $this->transaction->whereMonth('created_at',sprintf('%02s',$i))->whereYear('created_at',date('Y'))->get()->count();
-            $transaction_data [] = $lul;
+        $transaction = $this->transaction;
+        $transaction_data = collect([null]);
+        $year = date("Y");
+
+        $rvalue = Transaction::selectRaw("EXTRACT( YEAR_MONTH FROM rent_date) ym ,"
+            . " count(id) as total")
+            ->where(\DB::raw("Year(rent_date)"), $year)
+            ->groupBy("ym")
+            ->get();
+
+        $label = collect(['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli',
+            'Agustus', 'September', 'Oktober', 'November', 'Desember']);
+
+        if ($rvalue->count() > 0) {
+
+            $transaction_data = $label->map(function ($item, $key) use ($rvalue, $year) {
+                $d = $rvalue->where("ym", $year .
+                    str_pad($key + 1, 2, "0", STR_PAD_LEFT))->first();
+                if ($d) {
+                    return $d->total;
+                }
+                return 0;
+
+            });
+
+            //need to remove 0 value
+            $transaction_data = array_reverse($transaction_data->toArray(), false);
+
+            $pos = 0;
+            foreach ($transaction_data as $key => $value) {
+                if ($value > 0) {
+                    $pos = $key;
+                    break;}
+            }
+
+            $nullValues = array_fill(0, $pos, null);
+            $transaction_data = array_replace($transaction_data, $nullValues);
+            $transaction_data = array_reverse($transaction_data);
+            //return response()->json(["va" => $transaction_data]);
         }
-        $label = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
         $chartjs = app()->chartjs
-         ->name('trans')
-         ->type('line')
-         ->size(['width' => 400, 'height' => 200])
-         ->labels($label)
-         ->datasets([
-            [
-                "label" => "Transaksi",
-                'backgroundColor' => "rgba(78, 115, 223, 0.05)",
-                'borderColor' => "#e74a3b",
-                "pointHoverRadius" => "3",
-                "pointHitRadius"=> "10",
-                "pointBorderWidth"=> "2",
-                "pointBorderColor" => "#e74a3b",
-                "pointBackgroundColor" => "#e74a3b",
-                "pointHoverBackgroundColor" => "#e74a3b",
-                "pointHoverBorderColor" => "#e74a3b",
-                'data' => $transaction_data,
-                // 'data' => [1,2,3,4,5,6,7,8,9,10,11,12]
-            ]
-        ])
-         ->optionsRaw("{
+            ->name('trans')
+            ->type('line')
+            ->size(['width' => 400, 'height' => 200])
+            ->labels($label->toArray())
+            ->datasets([
+                [
+                    "label" => "Transaksi " . $year,
+                    'backgroundColor' => "rgba(78, 115, 223, 0.05)",
+                    'borderColor' => "#e74a3b",
+                    "pointHoverRadius" => "3",
+                    "pointHitRadius" => "10",
+                    "pointBorderWidth" => "2",
+                    "pointBorderColor" => "#e74a3b",
+                    "pointBackgroundColor" => "#e74a3b",
+                    "pointHoverBackgroundColor" => "#e74a3b",
+                    "pointHoverBorderColor" => "#e74a3b",
+                    'data' => $transaction_data,
+
+                ],
+            ])
+            ->optionsRaw("{
              'animation': {
                  'duration': 2000
              }
          }");
 
-        return view('backend.dashboard.index',compact(['car','customer','transaction','chartjs']));
+        return view('backend.dashboard.index', compact(['car', 'customer', 'transaction',
+            'chartjs']));
     }
 
 }
