b0a1cc25deb2caffdd8aa8cb114a5ee1a4490b5d
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2022-09-11 20:10:00 +0700

    add about page

---
+++ resources/views/backend/dashboard/about.blade.php
@@ -0,0 +1,10 @@
+@extends('backend.layouts')
+@section('title','Dashboard')
+@section('content')
+<pre>
+Source-code : https://github.com/ezra-dalimunthe/RentalMobil
+
+Forked from : https://github.com/sum4li/RentalMobil
+
+</pre>
+@endsection
